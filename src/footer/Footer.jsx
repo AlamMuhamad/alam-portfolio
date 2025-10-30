df02d7b54278775d2d0c4b70997d8a912407c3d4
Muhammad Alam <alambrohi11@gmail.com>
2025-10-30 10:27:52 +0500

    Fix CSS import casing to match actual filenames

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Footer.css";
+import "./footer.css";
 import { Github, Linkedin, Mail, Facebook, Instagram, Phone } from "lucide-react";
 
 const Footer = () => {
